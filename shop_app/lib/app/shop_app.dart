1e46a6887ef009b8b4b14a05fd80879e5f3117a2
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-09-24 03:59:54 -0400

    project configured, ready to get started

---
+++ shop_app/lib/app/shop_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ShopApp extends StatelessWidget {
+  ShopApp({Key? key}) : super(key: key);
+
+  @override
+  MaterialApp build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
