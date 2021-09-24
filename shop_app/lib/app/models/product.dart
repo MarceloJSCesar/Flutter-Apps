41f00c3c3a6f3b0b9767244d74cd48f8a8c5a0e7
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-09-24 04:06:47 -0400

    product class created

---
+++ shop_app/lib/app/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  bool? isFavorite;
+  final String? id;
+  final String? title;
+  final String? price;
+  final String? imageUrl;
+  final String? description;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+    required this.description,
+    this.isFavorite = false,
+  });
+}
