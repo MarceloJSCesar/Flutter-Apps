2be6e43a9e1ba5123e6443a9bb816be6cf9ac92c
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-09-26 18:41:43 -0400

    mobx implemented for darkTheme

---
+++ insta_reels/lib/src/controllers/home_controller.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$HomeController on _HomeControllerBase, Store {
+  final _$isDarkThemeAtom = Atom(name: '_HomeControllerBase.isDarkTheme');
+
+  @override
+  bool get isDarkTheme {
+    _$isDarkThemeAtom.reportRead();
+    return super.isDarkTheme;
+  }
+
+  @override
+  set isDarkTheme(bool value) {
+    _$isDarkThemeAtom.reportWrite(value, super.isDarkTheme, () {
+      super.isDarkTheme = value;
+    });
+  }
+
+  final _$_HomeControllerBaseActionController =
+      ActionController(name: '_HomeControllerBase');
+
+  @override
+  dynamic changeTheme() {
+    final _$actionInfo = _$_HomeControllerBaseActionController.startAction(
+        name: '_HomeControllerBase.changeTheme');
+    try {
+      return super.changeTheme();
+    } finally {
+      _$_HomeControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+isDarkTheme: ${isDarkTheme}
+    ''';
+  }
+}
--- insta_reels/lib/src/insta_reels.dart
+++ insta_reels/lib/src/insta_reels.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:insta_reels/src/controllers/home_controller.dart';
 
 class InstaReels extends StatelessWidget {
   InstaReels({Key? key}) : super(key: key);
+
+  bool get isDarkTheme => HomeController().isDarkTheme;
 
   @override
   MaterialApp build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+      ),
       home: Container(),
     );
   }
